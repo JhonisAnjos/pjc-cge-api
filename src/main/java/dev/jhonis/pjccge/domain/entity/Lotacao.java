83d9e0c162fad927f1c86eeb88c095dc77dfc897
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2022-06-08 23:23:34 -0300

    Entidade de domínio 'Lotacao' criada

---
+++ src/main/java/dev/jhonis/pjccge/domain/entity/Lotacao.java
@@ -0,0 +1,46 @@
+package dev.jhonis.pjccge.domain.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Lotacao implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Include
+	@Id
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "pessoa_id")
+	private Pessoa pessoa;
+	
+	@ManyToOne
+	@JoinColumn(name = "unidade_id")
+	private Unidade unidade;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataLotacao;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataRemocao;
+	
+	private String portaria;
+
+}
