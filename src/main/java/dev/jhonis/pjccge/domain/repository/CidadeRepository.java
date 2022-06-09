8ceeb4a2bf9ea1696ddf5ae9ac50f9b4285c3bd8
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2022-06-09 11:31:52 -0300

    CidadeRepository e EnderecoRepository criados

---
+++ src/main/java/dev/jhonis/pjccge/domain/repository/CidadeRepository.java
@@ -0,0 +1,9 @@
+package dev.jhonis.pjccge.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.jhonis.pjccge.domain.entity.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+
+}
---
+++ src/main/java/dev/jhonis/pjccge/domain/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package dev.jhonis.pjccge.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.jhonis.pjccge.domain.entity.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
+
+}
