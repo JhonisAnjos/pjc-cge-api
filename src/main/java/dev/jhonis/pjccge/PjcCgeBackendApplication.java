d25d685c5a397eedf8bebd62a86b54799e345bfd
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2022-06-08 21:35:45 -0300

    Primeiro commit

---
+++ src/main/java/dev/jhonis/pjccge/PjcCgeBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.jhonis.pjccge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjcCgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjcCgeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jhonis/pjccge/PjcCgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jhonis.pjccge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjcCgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
