6694965aadb7e0ba18e9dce9ff3f8a83b6dcffa1
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2022-06-10 17:28:29 -0300

    ServidorEfetivoController criado

---
+++ src/main/java/dev/jhonis/pjccge/api/controller/ServidorEfetivoController.java
@@ -0,0 +1,17 @@
+package dev.jhonis.pjccge.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.jhonis.pjccge.domain.ServidorEfetivoService;
+
+@RestController
+@RequestMapping(path = "/servidores-efetivos")
+public class ServidorEfetivoController {
+	
+	@Autowired
+	private ServidorEfetivoService servidorEfetivoService;
+	
+	
+}
