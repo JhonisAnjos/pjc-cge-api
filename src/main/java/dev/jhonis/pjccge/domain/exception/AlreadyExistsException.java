9dbf3208f157a180f183e6f617a30424e00d29f8
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2022-06-10 17:15:38 -0300

    AlreadyExistsException criada

---
+++ src/main/java/dev/jhonis/pjccge/domain/exception/AlreadyExistsException.java
@@ -0,0 +1,10 @@
+package dev.jhonis.pjccge.domain.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public AlreadyExistsException(String msg) {
+		super(msg);
+	}
+}
