e9e3098bb50618e040a69d54389b86796935e869
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2022-06-13 10:19:06 -0300

    ServidorEfetivoModel criado

---
+++ src/main/java/dev/jhonis/pjccge/api/model/ServidorEfetivoModel.java
@@ -0,0 +1,12 @@
+package dev.jhonis.pjccge.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ServidorEfetivoModel extends PessoaModel{
+
+	private String matricula;
+	
+}
